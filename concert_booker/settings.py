2943d162150c48593736c54ce7d780140b1ef83f
Daniel Lisø <daniel.lisoe@gmail.com>
2017-09-25 09:23:53 +0200

    Use crispy_forms

--- concert_booker/settings.py
+++ concert_booker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'accounts',
 ]
 
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+LOGOUT_REDIRECT_URL = '/'
