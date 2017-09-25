59431d007ac12329f3e5b8490d7539b8fe00178a
Daniel Lisø <daniel.lisoe@gmail.com>
2017-09-25 11:12:26 +0200

    Add band managers

--- bands/models.py
+++ bands/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 MAX_BANDNAME_LENGTH = 200
@@ -5,3 +6,4 @@ MAX_BANDNAME_LENGTH = 200
 
 class Band(models.Model):
     name = models.CharField(max_length=MAX_BANDNAME_LENGTH)
+    manager = models.ForeignKey(User, on_delete=models.CASCADE)
