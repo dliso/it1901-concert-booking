1153a43b203ebdc52c360bb08e8fda3eb07c2f78
Daniel Lisø <daniel.lisoe@gmail.com>
2017-09-25 11:52:49 +0200

    Handle user sign ups

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.shortcuts import render
 from django.views.generic import FormView, ListView
 
@@ -19,8 +20,32 @@ class Dashboard(ListView):
 class SignUpView(FormView):
     form_class = forms.SignUpForm
     template_name = 'registration/signup.html'
-    success_url = '.'
+
+    def get_success_url(self):
+        req = self.request
+        return req.GET.get('next', '/')
 
     def form_valid(self, form):
-        print(form.cleaned_data)
+        data = form.cleaned_data
+        # Create user
+        u = User.objects.create_user(data['username'], password=data['password1'])
+        u.save()
+        # Add user to groups
+        for group in data['groups']:
+            u.groups.add(group)
+
+        # TODO
+        # Create any requested new bands
+
+        # Add user as band manager of selected bands
+        for band in data['bands']:
+            band.manager = u
+            band.save()
+
+        # TODO
+        # Don't just hand over bands because people asked for it.
+
+        # Log user in
+        login(self.request, u)
+
         return super().form_valid(form)
