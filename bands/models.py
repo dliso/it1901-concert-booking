6759fcdbca718eb08d41148f323dfa5300838503
Daniel Lisø <daniel.lisoe@gmail.com>
2017-09-25 11:16:40 +0200

    Allow bands to not have managers

--- bands/models.py
+++ bands/models.py
@@ -6,4 +6,7 @@ MAX_BANDNAME_LENGTH = 200
 
 class Band(models.Model):
     name = models.CharField(max_length=MAX_BANDNAME_LENGTH)
-    manager = models.ForeignKey(User, on_delete=models.CASCADE)
+    manager = models.ForeignKey(User, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
