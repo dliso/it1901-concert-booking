0dda7929a63313125bea5bcea76260f0258a316d
Daniel Lisø <daniel.lisoe@gmail.com>
2017-09-25 11:25:42 +0200

    Show bands on dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,18 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import FormView, ListView
 
+from bands import models as band_models
+
 from . import forms
 
 
 class Dashboard(ListView):
     model = User
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['band_list'] = band_models.Band.objects.all()
+        return context
 
 
 class SignUpView(FormView):
