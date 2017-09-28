724d756d6999c7cbec556599ae42994800bd97df
Daniel Lisø <daniel.lisoe@gmail.com>
2017-09-28 09:39:08 +0200

    Add some model skeletons

--- bands/admin.py
+++ bands/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Band)
+admin.site.register(models.Genre)
+admin.site.register(models.Stage)
+admin.site.register(models.Concert)
--- bands/models.py
+++ bands/models.py
@@ -1,12 +1,32 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-MAX_BANDNAME_LENGTH = 200
+MAX_CHARFIELD_LENGTH_GENERAL = 200
+MAX_BANDNAME_LENGTH = MAX_CHARFIELD_LENGTH_GENERAL
+MAX_STAGENAME_LENGTH = MAX_CHARFIELD_LENGTH_GENERAL
 
 
 class Band(models.Model):
     name = models.CharField(max_length=MAX_BANDNAME_LENGTH)
     manager = models.ForeignKey(User, null=True, blank=True)
 
+    # This model has to be expanded to include at least genres.
+
     def __str__(self):
         return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=MAX_CHARFIELD_LENGTH_GENERAL)
+
+
+class Stage(models.Model):
+    name = models.CharField(max_length=MAX_STAGENAME_LENGTH)
+
+
+class Concert(models.Model):
+    name = models.CharField(max_length=MAX_CHARFIELD_LENGTH_GENERAL)
+
+    # This model has to be expanded to include which bands are playing, what
+    # stage it's happening on, technical requirements, who's performing
+    # technical duties.
