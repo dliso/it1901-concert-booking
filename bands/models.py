cb939450d8cdace7a6edd3378d5ecdd8848c8e9b
Ruanes <runar_espelund@outlook.com>
2017-10-09 14:35:06 +0200

    Added Technical Needs

--- bands/models.py
+++ bands/models.py
@@ -10,12 +10,18 @@ MAX_STAGENAME_LENGTH = MAX_CHARFIELD_LENGTH_GENERAL
 class Band(models.Model):
     name = models.CharField(max_length=MAX_BANDNAME_LENGTH)
     manager = models.ForeignKey(User, null=True, blank=True)
+    
 
     # This model has to be expanded to include at least genres.
 
     def __str__(self):
         return self.name
 
+class TechnicalNeed(models.Model):
+    concert_name = models.ForeignKey('Concert')
+    sound = models.TextField(blank = True)
+    light = models.TextField(blank = True)
+    other_technical_needs = models.TextField(blank = True)
 
 class Genre(models.Model):
     name = models.CharField(max_length=MAX_CHARFIELD_LENGTH_GENERAL)
