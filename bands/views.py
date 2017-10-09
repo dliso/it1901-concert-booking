2e1f1685af04e5e6ec9da2fdd45f20fa282370b6
sigurdnv <snv97@hotmail.com>
2017-10-09 14:15:56 +0200

    final changes to site where technicians can see their jobs

--- bands/views.py
+++ bands/views.py
@@ -10,7 +10,15 @@ class ConcertList(ListView):
     model = models.Concert
 
 class TeknikerList(ListView):
-    model = models.Concert
+    template_name = "bands/tekniker_list.html"
+    queryset = models.Concert.objects.all()
+    def get_template_names(self):
+        return self.template_name
+
+    def get_queryset(self):
+       user = self.request.user
+       users_concerts = models.Concert.objects.all()
+       return users_concerts
 
 class GenreList(ListView):
     model = models.Genre
