332f91d2d362c0b997e62c59f0ee734d62b6bcaa
Daniel Lisø <daniel.lisoe@gmail.com>
2017-10-23 11:02:32 +0200

    Move SearchForm to forms.py

---
+++ bands/forms.py
@@ -0,0 +1,23 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import HTML, BaseInput, Div, Field, Layout, Submit
+from django import forms
+
+from . import models
+
+
+class SearchForm(forms.Form):
+    query = forms.fields.CharField()
+    stage = forms.ModelMultipleChoiceField(
+        models.Stage.objects,
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'query',
+            'stage'
+        )
+        self.helper.add_input(Submit('submit', 'Submit'))
