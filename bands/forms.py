fc16365cdc9e28cab01f5ba52bb04b337cf643cc
Daniel Lisø <daniel.lisoe@gmail.com>
2017-10-23 11:17:19 +0200

    Allow empty search queries

--- bands/forms.py
+++ bands/forms.py
@@ -6,7 +6,7 @@ from . import models
 
 
 class SearchForm(forms.Form):
-    query = forms.fields.CharField()
+    query = forms.fields.CharField(required=False)
     stage = forms.ModelMultipleChoiceField(
         models.Stage.objects,
         widget=forms.CheckboxSelectMultiple,
