91b63f11c0c6f03692bea524e8427afb374b6cf0
Daniel Lisø <daniel.lisoe@gmail.com>
2017-11-02 14:18:33 +0100

    Fix some small bugs

--- static/js/concert_price.js
+++ static/js/concert_price.js
@@ -3,7 +3,6 @@ const print = console.log;
 const form = document.getElementById("create-concert-form");
 
 function handleFormChange(event) {
-  const priceField = document.getElementById("id_ticket_price");
   const stage_info_string = document.getElementById("stage_info").innerHTML;
   const stage_info = JSON.parse(stage_info_string);
   const stage_info_map = new Map(stage_info.map(stage=>{
@@ -13,7 +12,6 @@ function handleFormChange(event) {
   const stageField = document.getElementById("id_stage_name");
   const selectedStage = stage_info_map.get(stageField.value);
     if (event.target !== priceField) {
-        priceField.value = 100;
         priceField.value = (selectedStage.fields.stage_costs*1.5)/selectedStage.fields.num_seats;
       //Formula calculating an example ticket prize
       //(Total costs + profit)/Total tickets
